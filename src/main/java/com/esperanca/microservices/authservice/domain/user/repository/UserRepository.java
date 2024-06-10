dae5e809714afc9e0d532647e01dc06269997ef9
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-10 20:29:36 -0300

    Introduced UserRepository interface to manage user data within the application. This interface extends JpaRepository to inherit basic CRUD operations from Spring Data JPA. It includes a method to find UserDetails by username, facilitating user authentication and authorization processes.

---
+++ src/main/java/com/esperanca/microservices/authservice/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.esperanca.microservices.authservice.domain.user.repository;
+
+import com.esperanca.microservices.authservice.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  UserDetails findByUsername(String username);
+}
