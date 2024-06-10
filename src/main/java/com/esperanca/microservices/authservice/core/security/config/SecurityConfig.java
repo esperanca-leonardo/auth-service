acd3229f06ea07a5618f9c4dc4186f308902ab62
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-10 20:56:43 -0300

    Introduced SecurityConfig class to configure security settings within the application. This class: - Configures Spring Security to disable CSRF protection, use stateless session management, and disable exception handling. - Defines a SecurityFilterChain bean to customize the security filter chain. - Defines a bean for configuring the AuthenticationManager to be used for authentication. - Defines a bean for creating BCryptPasswordEncoder for password encoding. - Annotated with Spring's @Configuration and @EnableWebSecurity to enable web security configuration.

---
+++ src/main/java/com/esperanca/microservices/authservice/core/security/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.esperanca.microservices.authservice.core.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+        .exceptionHandling(AbstractHttpConfigurer::disable)
+        .build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
