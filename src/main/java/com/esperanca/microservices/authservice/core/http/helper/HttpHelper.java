8dfe7c5cf83f01dff65fd0d16923c2be394f64e6
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-10 20:15:33 -0300

    Added HttpHelper interface to define methods for HTTP operations, including: - createUri: Constructs a URI from host, port, endpoint, and query parameters. - buildRequest: Builds an HTTP request from a given URI. - sendRequest: Sends an HTTP request and handles possible exceptions (HttpRequestException, HttpInterruptedException).

---
+++ src/main/java/com/esperanca/microservices/authservice/core/http/helper/HttpHelper.java
@@ -0,0 +1,20 @@
+package com.esperanca.microservices.authservice.core.http.helper;
+
+import com.esperanca.microservices.authservice.core.http.exceptions.HttpInterruptedException;
+import com.esperanca.microservices.authservice.core.http.exceptions.HttpRequestException;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public interface HttpHelper {
+
+  URI createUri(String host, String port, String endpoint, String key,
+      String value
+  );
+
+  HttpRequest buildRequest(URI uri);
+
+  HttpResponse<String> sendRequest(HttpRequest request)
+      throws HttpRequestException, HttpInterruptedException;
+}
