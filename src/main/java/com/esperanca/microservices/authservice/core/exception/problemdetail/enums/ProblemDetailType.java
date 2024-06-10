e5c42ae4418ab2911ec8247c0dc0e28d3974e32a
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-10 20:26:50 -0300

    Introduced ProblemDetailType enum to define types of problem details that can occur within the application. This enum includes: - INVALID_DATA: Represents invalid data errors with a URI, title, and HTTP status code. - USER_NOT_FOUND: Represents user not found errors with a URI, title, and HTTP status code. - BAD_CREDENTIALS: Represents bad credentials errors with a URI, title, and HTTP status code.
    Each enum constant encapsulates a URI, a title, and an HTTP status code to provide standardized error responses across the application.

---
+++ src/main/java/com/esperanca/microservices/authservice/core/exception/problemdetail/enums/ProblemDetailType.java
@@ -0,0 +1,26 @@
+package com.esperanca.microservices.authservice.core.exception.problemdetail.enums;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Getter
+public enum ProblemDetailType {
+  INVALID_DATA("/invalid-data", "Invalid data", BAD_REQUEST),
+  USER_NOT_FOUND("/user-not-found", "User not found", NOT_FOUND),
+  BAD_CREDENTIALS("/bad-credentials", "Bad credentials", BAD_REQUEST);
+
+  private final URI type;
+  private final String title;
+  private final HttpStatus status;
+
+  ProblemDetailType(String type, String title, HttpStatus status) {
+    this.title = title;
+    this.status = status;
+    this.type = URI.create("https://authservice.com.br" + type);
+  }
+}
