846a1957b5e7800c41c8917ae44a7b53a5b77ea7
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-12 17:58:27 -0300

    Added Swagger annotations and validation messages to SignInDto fields.

--- src/main/java/com/esperanca/microservices/authservice/domain/user/dtos/SignInDto.java
+++ src/main/java/com/esperanca/microservices/authservice/domain/user/dtos/SignInDto.java
@@ -1,17 +1,22 @@
 package com.esperanca.microservices.authservice.domain.user.dtos;
 
 import com.esperanca.microservices.authservice.domain.user.enums.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class SignInDto {
 
+  @Schema(example = "user")
   @NotBlank(message = "username cannot be blank")
   private String username;
 
+  @Schema(example = "123")
   @NotBlank(message = "password cannot be blank")
   private String password;
 
+  @Schema(example = "ADMIN")
+  @NotBlank(message = "role cannot be blank")
   private Role role;
 }
