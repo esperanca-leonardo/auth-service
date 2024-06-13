d7fb28ea26b6831e88b43efeee4abbf67a22f0db
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-12 21:52:16 -0300

    Added Swagger annotations to LoginDto fields for better API documentation.

--- src/main/java/com/esperanca/microservices/authservice/domain/user/dtos/LoginDto.java
+++ src/main/java/com/esperanca/microservices/authservice/domain/user/dtos/LoginDto.java
@@ -1,14 +1,17 @@
 package com.esperanca.microservices.authservice.domain.user.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginDto {
 
+  @Schema(example = "user")
   @NotBlank(message = "username cannot be blank")
   private String username;
 
+  @Schema(example = "123")
   @NotBlank(message = "password cannot be blank")
   private String password;
 }
