91dcae746f819904dacf53beaf3ef93d7fd78bd7
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-13 18:42:11 -0300

    Refactored the generateToken method signature in the TokenClient interface.

--- src/main/java/com/esperanca/microservices/authservice/domain/token/client/TokenClient.java
+++ src/main/java/com/esperanca/microservices/authservice/domain/token/client/TokenClient.java
@@ -2,5 +2,5 @@ package com.esperanca.microservices.authservice.domain.token.client;
 
 public interface TokenClient {
 
-  String generateToken(String username) throws RuntimeException;
+  String generateToken(String username);
 }
