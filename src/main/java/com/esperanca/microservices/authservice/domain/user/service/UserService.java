a17550d047fbd6c990ee364fec29598fb3467a6c
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-13 19:19:00 -0300

    Added a method to check if a username already exists in the UserService interface, and implemented it in the UserService class.

--- src/main/java/com/esperanca/microservices/authservice/domain/user/service/UserService.java
+++ src/main/java/com/esperanca/microservices/authservice/domain/user/service/UserService.java
@@ -1,39 +1,12 @@
 package com.esperanca.microservices.authservice.domain.user.service;
 
 import com.esperanca.microservices.authservice.domain.user.entity.User;
-import com.esperanca.microservices.authservice.domain.user.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import com.esperanca.microservices.authservice.domain.user.exception.UsernameAlreadyExistsException;
 
-import static java.lang.String.format;
-import static java.util.Objects.isNull;
+public interface UserService {
 
-@Service
-@AllArgsConstructor
-public class UserService implements UserDetailsService {
+  User save(final User user);
 
-  private final UserRepository repository;
-
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    final UserDetails userDetails = this.repository.findByUsername(username);
-
-    if (isNull(userDetails)) {
-      throw new UsernameNotFoundException(
-          format("User not found with username: %s", username)
-      );
-    }
-    return userDetails;
-  }
-
-  public boolean existsByUsername(String username) {
-    return this.repository.existsByUsername(username);
-  }
-
-  public User save(User user) {
-    return this.repository.save(user);
-  }
+  void existsByUsername(final String username)
+      throws UsernameAlreadyExistsException;
 }
