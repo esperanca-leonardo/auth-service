ed111f83539c3eea04016f03e9cc3f7ca7144dec
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-13 20:14:55 -0300

    Removed throws IOException and InterruptedException signature exception

--- src/main/java/com/esperanca/microservices/authservice/domain/token/service/TokenService.java
+++ src/main/java/com/esperanca/microservices/authservice/domain/token/service/TokenService.java
@@ -1,8 +1,6 @@
 package com.esperanca.microservices.authservice.domain.token.service;
 
-import java.io.IOException;
-
 public interface TokenService {
 
-  String generateToken(String username) throws IOException, InterruptedException;
+  String generateToken(final String username);
 }
--- src/main/java/com/esperanca/microservices/authservice/domain/token/service/TokenServiceImpl.java
+++ src/main/java/com/esperanca/microservices/authservice/domain/token/service/TokenServiceImpl.java
@@ -4,8 +4,6 @@ import com.esperanca.microservices.authservice.domain.token.client.TokenClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 @AllArgsConstructor
 public class TokenServiceImpl implements TokenService {
@@ -13,7 +11,7 @@ public class TokenServiceImpl implements TokenService {
   private final TokenClient tokenClient;
 
   @Override
-  public String generateToken(String username) throws IOException, InterruptedException {
-    return this.tokenClient.generateToken(username);
+  public String generateToken(final String username) {
+    return this.tokenClient.getTokenFromService(username);
   }
 }
